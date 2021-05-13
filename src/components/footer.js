883e8f28a6014ee1e27489878dd461a8789b266f
MuhamadTaha <motaha72@outlook.com>
2021-05-13 12:21:40 +0200

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,63 +3,67 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faInstagram, faYoutube, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { faMapMarkerAlt, faSchool, faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                <div className='container'>
-                    <div class="row justify-content-center">
+import { useTranslation } from "react-i18next";
 
-                        <div class="col-md-4 col-11 contact-us">
-                            <h4> Contact Us </h4>
-                            <div class="contact-us-element">
-                                <FontAwesomeIcon icon={faSchool}> </FontAwesomeIcon>
-                                <p> Nile Egyptian Schools </p>
-                            </div>
+function Footer() {
 
-                            <div class="contact-us-element">
-                                <FontAwesomeIcon icon={faMapMarkerAlt}> </FontAwesomeIcon>
-                                <p> 27 Mossadak, Dokki, Giza, Egypt </p>
-                            </div>
+    const { t } = useTranslation();
+    return (
+        <footer>
+            <div className='container'>
+                <div class="row justify-content-center">
 
-                            <div class="contact-us-element">
-                                <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>
-                                <p> 02 33389870 </p>
-                            </div>
-
-                            <div class="contact-us-element">
-                                <FontAwesomeIcon icon={faEnvelope}> </FontAwesomeIcon>
-                                <p> admission@nes.moe.edu.eg </p>
-                            </div>
+                    <div class="col-md-4 col-11 contact-us">
+                        <h4> {t('footer_contact_title')} </h4>
+                        <div class="contact-us-element">
+                            <FontAwesomeIcon icon={faSchool}> </FontAwesomeIcon>
+                            <p> {t('footer_contact_school_name')} </p>
                         </div>
 
-                        <div class="col-md-4 col-11 brief">
-                            <h4> NES Brief </h4>
-                            <p> Our vision is to play a leading role in transforming teaching &emp; learning culture through providing innovative cutting-edge pedagogical (teaching and learning) approaches and use of partnerships, and technology. </p>
+                        <div class="contact-us-element">
+                            <FontAwesomeIcon icon={faMapMarkerAlt}> </FontAwesomeIcon>
+                            <p> {t('footer_contact_address')} </p>
                         </div>
 
-                        <div class="col-md-4 col-11 contact-us">
-                            <h4> Keep in Touch </h4>
-                            <div class="footer-social">
-                                <a href="https://www.facebook.com/nesofficial1" target="_blank">
-                                    <FontAwesomeIcon icon={faFacebookF}> </FontAwesomeIcon>
-                                </a>
-                                <a href="https://www.instagram.com/nesofficial1/" target="_blank">
-                                    <FontAwesomeIcon icon={faInstagram}> </FontAwesomeIcon>
-                                </a>
-                                <a href="https://www.youtube.com/" target="_blank">
-                                    <FontAwesomeIcon icon={faYoutube}> </FontAwesomeIcon>
-                                </a>
-                                <a href="https://www.linkedin.com" target="_blank">                                     <FontAwesomeIcon icon={faLinkedinIn}> </FontAwesomeIcon>
-                                </a>
-                            </div>
+                        <div class="contact-us-element">
+                            <FontAwesomeIcon icon={faPhoneAlt}> </FontAwesomeIcon>
+                            <p> 02 33389870 </p>
                         </div>
 
+                        <div class="contact-us-element">
+                            <FontAwesomeIcon icon={faEnvelope}> </FontAwesomeIcon>
+                            <p> admission@nes.moe.edu.eg </p>
+                        </div>
                     </div>
+
+                    <div class="col-md-4 col-11 brief">
+                        <h4>{t('footer_brief_title')}</h4>
+                        <p>{t('footer_brief_desc')} </p>
+                    </div>
+
+                    <div class="col-md-4 col-11 contact-us">
+                        <h4> {t('footer_touch_title')} </h4>
+                        <div class="footer-social">
+                            <a href="https://www.facebook.com/nesofficial1" target="_blank">
+                                <FontAwesomeIcon icon={faFacebookF}> </FontAwesomeIcon>
+                            </a>
+                            <a href="https://www.instagram.com/nesofficial1/" target="_blank">
+                                <FontAwesomeIcon icon={faInstagram}> </FontAwesomeIcon>
+                            </a>
+                            <a href="https://www.youtube.com/" target="_blank">
+                                <FontAwesomeIcon icon={faYoutube}> </FontAwesomeIcon>
+                            </a>
+                            <a href="https://www.linkedin.com" target="_blank">
+                                <FontAwesomeIcon icon={faLinkedinIn}> </FontAwesomeIcon>
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
-            </footer>
-        )
-    }
+            </div>
+        </footer>
+    )
 }
+
 
 export default Footer;
