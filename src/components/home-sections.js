f1328a1c80cac8e229c2a6848fa3c4f1c1ad28f6
MuhamadTaha <motaha72@outlook.com>
2021-06-17 03:48:37 +0200

    Update home-sections.js

--- src/components/home-sections.js
+++ src/components/home-sections.js
@@ -4,15 +4,45 @@ import { useTranslation } from "react-i18next";
 
 function Home_Sections() {
     const { t } = useTranslation();
-        return (
-            <section id='home'>
-                <div className='container'>
-                    <h1> {t('home_text')} </h1>
-                    <Header />
+    return (
+        <section id='home'>
+            <div className='container'>
+
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://i.ibb.co/z5p8Vqh/slider1.jpg" alt="school-1" border="0" width='100%' />
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://i.ibb.co/2ZQ9HwQ/slider2.jpg" alt="school-1" border="0" width='100%' />
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://i.ibb.co/1GSHdz5/slider3.jpg" alt="school-1" border="0" width='100%' />
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://i.ibb.co/fSNh0BL/slider4.jpg" alt="school-1" border="0" width='100%' />
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://i.ibb.co/x77wdCh/slider6.jpg" alt="school-1" border="0" width='100%' />
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-            </section>
-        )
-    }
+
+                <h1 className='sec_title'> {t('home_text')} - {t('footer_contact_school_name')} </h1>
+
+                <Header />
+            </div>
+        </section>
+    )
+}
 
 
 export default Home_Sections;
